34a5bbb3d3532973e4fb17e363176f308da8a162
ElvinWeb <izafact-bp215@code.edu.az>
2025-04-11 11:11:54 +0400

    Reflection

---
+++ Refleksiya/Program.cs
@@ -0,0 +1,32 @@
+﻿using Refleksiya;
+using System.Reflection;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Type usertype = typeof(User);
+        User user1 = (User)Activator.CreateInstance(usertype);
+
+        FieldInfo field1 = usertype.GetField("ID", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+        field1.SetValue(user1, 1);
+        FieldInfo field2 = usertype.GetField("Name", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+        field2.SetValue(user1, "Elsad");
+        FieldInfo field3 = usertype.GetField("SurName", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+        field3.SetValue(user1, "Rzazade");
+        FieldInfo field4 = usertype.GetField("age", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+        field4.SetValue(user1, 20);
+
+
+
+        MethodInfo methodInfo = usertype.GetMethod("GetFullNme");
+        methodInfo.Invoke(user1, null);
+
+        MethodInfo method = usertype.GetMethod("ChangeAge");
+        method.Invoke(null, new object[] {25 } );
+     
+
+
+
+    }
+}
---
+++ Refleksiya/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refleksiya
+{
+    public class User
+    {
+        public string Name;
+        public string SurName;
+        public int ID;
+        public static int age;
+        public void GetFullNme()
+        {
+            Console.WriteLine(Name + " "+ SurName);
+        }
+        public static void ChangeAge(int Newage)
+        {
+            age = Newage;
+            Console.WriteLine(age);
+        }
+    }
+}
